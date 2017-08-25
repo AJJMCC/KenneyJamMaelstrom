122a23488a975a8a2792befa2affd6bc5a2df51c
SAE\amcclenaghan <1004436@student.sae.edu.au>
2017-08-25 17:28:30 +1000

    start

---
+++ Attempt1/Assets/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public static PlayerController Instance;
+
+
+
+    public float TurnSpeed;
+    public float BoostSpeed;
+
+
+    //private float TurningLeft;
+    //private float TurningRight;
+
+	// Use this for initialization
+	void Start () {
+        Instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        CheckTurns();
+        CheckThrust();
+	}
+
+    void CheckTurns()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0,0,1 * TurnSpeed * Time.deltaTime) ;
+        }
+
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0, 0, 1 * -TurnSpeed * Time.deltaTime);
+        }
+    }
+
+    void CheckThrust()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.GetComponent<Rigidbody>().AddForce(transform.up * BoostSpeed * Time.deltaTime);
+        }
+    }
+
+    
+}
